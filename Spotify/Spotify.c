12460fc52129ec53d27c53415a4b3b065958164d
Bongio03 <72200905+Bongio03@users.noreply.github.com>
2021-03-25 16:09:11 +0100

    Update Spotify.c

--- Spotify/Spotify.c
+++ Spotify/Spotify.c
@@ -3,32 +3,32 @@ Author:Bongiovanni Daniele
 4^Arob
 */
 
-#include <stdlib.h>
+#include <stdlib.h>                
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
-#define LUNG 25
-#define N_CANZONI 11
+#define LUNG 25                     //Numero massimo di cartteri ipotizzato per i titoli delle canzoni e per i nomi degli artisti
+#define N_CANZONI 11                //Numero di canzoni presenti nel CSV
 
-typedef struct canzone{
+typedef struct canzone{             //Struttura delle canzoni
     int num;
     char nome_canzone[LUNG];
     char artista[LUNG];
     int ord;
 }Canzone;
 
-void visualizzaPlaylist(Canzone play[],int num){
+void visualizzaPlaylist(Canzone play[],int num){        //Funzione che ricevendo un vettore di Canzoni, ne stampa il contenuto 
     for(int k=0; k<num; k++){
         printf("->%d %s - %s\n",play[k].num,play[k].nome_canzone,play[k].artista);
     }
 }
 
-void letturaCanzArt(FILE *f,Canzone p[], int num){
+void letturaCanzArt(FILE *f,Canzone p[], int num){      //Lettura del file CSV.
     int cnt=0;
-    char carattere, strProv[LUNG];
+    char carattere, strProv[LUNG];                      //strProv è un vettore dove vengono salvate provvisoriamente i cartteri letti.
 
-    //Lettura del nome della canzone
+    //Lettura del nome della canzone. La lettura avviene carattere per carattere.La lettura del nome della canzone termina quando viene letto il carattere ","
     fscanf(f,"%c",&carattere);
     while(carattere!=','){
         strProv[cnt]=carattere;
@@ -36,12 +36,12 @@ void letturaCanzArt(FILE *f,Canzone p[], int num){
         fscanf(f,"%c",&carattere);
     }
     strProv[cnt]='\0';
-    strcpy(p[num].nome_canzone,strProv);
+    strcpy(p[num].nome_canzone,strProv);                
 
     cnt=0;
-    memset(strProv,0,LUNG);
+    memset(strProv,0,LUNG);                             //Viene resettato strProv per la lettura del nome dell'artista//
 
-    //Lettura del nome dell artista
+    //Lettura del nome dell artista. La lettura avviene nello stesso modo della lettura del titolo della canzone
     fscanf(f,"%c",&carattere);
     while(carattere!='\n'){
         strProv[cnt]=carattere;
@@ -52,8 +52,8 @@ void letturaCanzArt(FILE *f,Canzone p[], int num){
     strcpy(p[num].artista,strProv);
 }
 
-void randomRiproduzione(Canzone p[], int num){
-    int sort,vNumSort[LUNG];
+void randomRiproduzione(Canzone p[], int num){          //Funzione per ordinare in modo randomico le canzoni 
+    int sort,vNumSort[LUNG];                            //sort è il numero che viene sorteggiato. vNumSort è il vettore dove vengono salvati i numeri già sorteggiati. 
     bool uguale;
 
     srand(time(NULL));
@@ -70,19 +70,18 @@ void randomRiproduzione(Canzone p[], int num){
         }while(uguale==true);
         vNumSort[k]=sort;
     }
-    for(int j=0;j<num;j++){
+    for(int j=0;j<num;j++){                             //Stampa delle canzoni in ordine randomico
         printf("->%d %s - %s\n",p[vNumSort[j]].num,p[vNumSort[j]].nome_canzone,p[vNumSort[j]].artista);
     }
 }
 
 int main(){
     FILE *fp;
-    Canzone playlist[N_CANZONI];
-    Canzone riproduzione[N_CANZONI];
-    int cntNumCanz,cnt;
-    char carattere,canzProv[LUNG],artProv[LUNG];
+    Canzone playlist[N_CANZONI];                        //Vettore per le canzoni lette nel ordine del CSV
+    Canzone riproduzione[N_CANZONI];                    //Vettore per le canzoni ordinate in modo randomico
+    int cntNumCanz,cnt;                                 
 
-    if((fp=fopen("canzoni.csv","r"))==NULL){
+    if((fp=fopen("canzoni.csv","r"))==NULL){            //Apertura del file CSV
         printf("File non esistente");
     }else{
         cntNumCanz=0;//contatore delle canzone inserite
