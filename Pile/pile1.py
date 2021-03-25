a7af83442faa4d86a0902b1fad750a34404f85c8
Bongio03 <72200905+Bongio03@users.noreply.github.com>
2021-03-25 17:44:15 +0100

    Add files via upload

---
+++ Pile/pile1.py
@@ -0,0 +1,15 @@
+def push_(stack, element):
+    stack.append(element)
+
+def pop_(stack):
+    return stack.pop()
+
+def main():
+    pila=[]
+    num=str(input("Inserire il numero ì da invertire:"))
+    print(num)
+    for k in num:
+        numAppoggio= int(num)/%
+        
+if __name__ == "__main__":
+    main()
