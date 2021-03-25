ca787365d1c95c3555f13dcfb759393af4116875
Bongio03 <72200905+Bongio03@users.noreply.github.com>
2021-03-25 18:21:44 +0100

    Update Code1.c

--- Code/Code1.c
+++ Code/Code1.c
@@ -9,24 +9,24 @@ testare la funzione dequeue per svuotare la coda.
 #include <stdbool.h>
 #include <string.h>
 
-typedef struct el{
+typedef struct el{                
     int valore;
     struct el* next;
 }El;
 
-int isEmpty(El *struttura){
+int isEmpty(El *struttura){                         //Funzione per vedere se la struttura è vuota o meno
     if(struttura == NULL) return 1;
     return 0;
 }
 
-void enqueue(El** head,El** tail,El* element){
+void enqueue(El** head,El** tail,El* element){      //Funzione per il caricamento di una coda
     if(isEmpty(*head)) *head = element;
     else    (*tail)->next = element;
     *tail = element;
     element->next = NULL;
 }
 
-El* dequeue(El** head,El** tail){
+El* dequeue(El** head,El** tail){                   //Funzione per svuotare la coda di un elemento
     El* ret = *head;
 
     if(isEmpty(*head)) return NULL;
@@ -38,23 +38,23 @@ El* dequeue(El** head,El** tail){
 
 
 int main(){
-    El* head;
+    El* head;                                       
     El* tail;
     El* input;
     
-    head=NULL;
+    head=NULL;                                      //Essendo inizialmente vuota la coda, viene assegnato NULL alla testa e alla coda
     tail=NULL;
-    
-    do{
-        input= (El*) malloc(sizeof(El));
+        
+    do{                                             //Ciclo per l'inserimento degli imnput
+        input= (El*) malloc(sizeof(El));            //Viene allocato lo spazio per ricevere l'input
         printf("Inserire un numero intero a piacere(Inseire -1 per terminare): ");
         fflush(stdin);
         scanf("%d",&(input->valore));
-        enqueue(&head,&tail,input);
+        enqueue(&head,&tail,input);                 //L'elemento viene caricato nella coda
     }while(tail->valore!=-1);
     
 
-    while(isEmpty(head->next)==0){ 
+    while(isEmpty(head->next)==0){                 //Stampa della coda. Durante la stampa viene anche svuotata la coda
         printf("%d",(dequeue(&head,&tail))->valore);
     }
     return 0;
